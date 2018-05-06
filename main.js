2c67b98460d8136da7f422707638f9739f8b0509
Dimitar Kokotanekov <acid901@mail.bg>
2018-05-06 17:57:10 +0300

    added checkAnswer prototype

--- main.js
+++ main.js
@@ -1,31 +1,46 @@
-
-var Questions = function(question, answers, correct){
-    this.question = question;
-    this.answers = answers;
-    this.correct = correct;
-}
-
-
-Questions.prototype.displayQuestion = function(){
-
-    console.log(this.question);
-    for(let i = 0; i < this.answers.length; i++){
-        console.log(i + ': ' + this.answers[i]);
+(function(){
+    var Questions = function(question, answers, correct){
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
     }
-}; 
+    
+    
+    Questions.prototype.displayQuestion = function(){
+    
+        console.log(this.question);
+        for(let i = 0; i < this.answers.length; i++){
+            console.log(i + ': ' + this.answers[i]);
+        }
+    }; 
+    
+    Questions.prototype.answerCheck = function(ans){
+        
+            if(ans === this.correct){
+                console.log('Correct!')
+            }else{
+                console.log('WRONG!')
+            }
+    
+    }
+    
+    let q1 = new Questions('My name?', ['Mitko', 'Anio', 'Dont know'],0);
+    let q2 = new Questions('My favourite colooo?', ['blue', 'red', 'pink'], 2);
+    let q3 = new Questions('My favourite color?', ['blue', 'red', 'pink'], 1);
+    
+    
+    let questions = [q1, q2, q3];
+    
+    let n = Math.floor( Math.random()* questions.length);
+    
+    questions[n].displayQuestion();
+    let answer = parseInt(prompt('Your asnwer?'));
+    
+    questions[n].answerCheck(answer);
+})();
 
-let q1 = new Questions('My name?', ['Mitko', 'Anio', 'Dont know'],0);
-let q2 = new Questions('My favourite colooo?', ['blue', 'red', 'pink'], 2);
-let q3 = new Questions('My favourite color?', ['blue', 'red', 'pink'], 1);
 
 
-let questions = [q1, q2, q3];
 
-let n = Math.floor( Math.random()* questions.length);
-
-questions[n].displayQuestion();
-
-
-let answer = prompt('Your asnwer?');
 
 
