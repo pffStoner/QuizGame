6856900fa1f634e9c8479acfdd8cf6584f864850
Dimitar Kokotanekov <acid901@mail.bg>
2018-05-16 22:18:39 +0300

    added next question, score

--- main.js
+++ main.js
@@ -12,31 +12,57 @@
         for(let i = 0; i < this.answers.length; i++){
             console.log(i + ': ' + this.answers[i]);
         }
-    }; 
+    } 
+
+    Questions.prototype.displayScore = function(score){
+        console.log('Your score is: ' + score);
+        console.log('--------------------------------------');
+    }
     
-    Questions.prototype.answerCheck = function(ans){
-        
+    Questions.prototype.answerCheck = function(ans, callback){
+        let sc;
             if(ans === this.correct){
                 console.log('Correct!')
+                sc = callback(true);
             }else{
                 console.log('WRONG!')
+                sc = callback(false);
             }
-    
+        this.displayScore(sc); 
     }
     
     let q1 = new Questions('My name?', ['Mitko', 'Anio', 'Dont know'],0);
     let q2 = new Questions('My favourite colooo?', ['blue', 'red', 'pink'], 2);
     let q3 = new Questions('My favourite color?', ['blue', 'red', 'pink'], 1);
-    
-    
     let questions = [q1, q2, q3];
+
+   function score() {
+       let sc = 0;
+       return function(correct) {
+           if(correct){
+               sc++;
+           }
+           return sc;
+       }
+   }
+
+    var keepScore = score();
+
+    function nextQuestion() {
     
-    let n = Math.floor( Math.random()* questions.length);
-    
-    questions[n].displayQuestion();
-    let answer = parseInt(prompt('Your asnwer?'));
-    
-    questions[n].answerCheck(answer);
+        let n = Math.floor( Math.random()* questions.length);
+        
+        questions[n].displayQuestion();
+        let answer = prompt('Your asnwer?');
+
+       if (answer !== 'exit') {
+        questions[n].answerCheck(parseInt(answer),keepScore);
+        nextQuestion();  
+        console.log(answer);    
+       } 
+    }
+
+    nextQuestion();
 })();
 
 
